417910fd03770930e727ee57fa59c2efeab79aec
sureshkumar198 <suresh.shyamalas@gmail.com>
2022-10-07 20:48:56 +0100

    first commit

---
+++ Calculator2.java
@@ -0,0 +1,18 @@
+class Calculator2
+{
+public static void main(String[] args)
+{
+Calculator2  calc = new Calculator2();
+calc.add(10,20);
+calc.add();
+}
+void add (int no1 , int no2)
+{
+System.out.println(10+20);
+}
+void add()
+{
+System.out.println(30+40);
+}
+}
+
